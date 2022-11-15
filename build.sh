29f458e2cd10f11ca72410ee63d04e8e96f9fac2
liang <xing.liang@greatdb.com>
2022-11-15 15:20:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
+export PATH=$PATH:/usr/local/go/bin
+export GO111MODULE=on
+export GOPROXY=https://goproxy.cn
+export CXXFLAGS="-stdlib=libstdc++" CC=/usr/bin/gcc CXX=/usr/bin/g++
+
 vs=`cat ./inputArg/flagHelp.go| grep "app.Version"|awk -F "=" '{print $2}'|sed 's/\"//g'|sed 's/\/\/版本//g'|sed 's/ //g'`
+
 go build -o gt-checkOut greatdbCheck.go
 mkdir gt-checkOut-${vs}-linux-x86-64
 cp -rpf gt-checkOut gc.conf gt-checkOut-${vs}-linux-x86-64
