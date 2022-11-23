15ea45f01e6fd2227a62c7aa12c45dcb9760d54a
liang <xing.liang@greatdb.com>
2022-11-23 11:00:29 +0800

    Update build-x86.sh

--- build-x86.sh
+++ build-x86.sh
@@ -9,11 +9,12 @@ vs=`cat ./inputArg/flagHelp.go| grep "app.Version"|awk -F "=" '{print $2}'|sed '
 if [ ! -d "/usr/lcoal/instantclient_19_17" ];then
   cp -rpf Oracle/instantclient_19_17 /usr/lcoal/
 fi
-export LD_LIBRARY_PATH=/usr/local/instantclient_11_2:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/instantclient_19_17:$LD_LIBRARY_PATH
 
 go build -o gt-checkOut greatdbCheck.go
 mkdir gt-checkOut-${vs}-linux-x86-64
 cp -rpf gt-checkOut gc.conf gt-checkOut-${vs}-linux-x86-64
+cp -rpf Oracle/instantclient_19_17 gt-checkOut-${vs}-linux-x86-64
 tar zcf gt-checkOut-${vs}-linux-x86-64.tar.gz gt-checkOut-${vs}-linux-x86-64
 mkdir binary
 mv gt-checkOut-${vs}-linux-x86-64.tar.gz binary
