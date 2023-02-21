b664ad4b39b5c3a4ef9ae2dabd92f4707f880adc
hang <xing.liang@greatdb.com>
2023-02-21 11:07:38 +0800

    v 1.1.10

--- build-x86.sh
+++ build-x86.sh
@@ -6,16 +6,17 @@ export GOPROXY=https://goproxy.cn
 export CXXFLAGS="-stdlib=libstdc++" CC=/usr/bin/gcc CXX=/usr/bin/g++
 
 vs=`cat ./inputArg/flagHelp.go| grep "app.Version"|awk -F "=" '{print $2}'|sed 's/\"//g'|sed 's/\/\/版本//g'|sed 's/ //g'`
-if [ ! -d "/usr/lcoal/instantclient_19_17" ];then
-  cp -rpf Oracle/instantclient_19_17 /usr/lcoal/
+OracleDrive="instantclient_11_2"
+if [ ! -d "/usr/lcoal/${OracleDrive}" ];then
+  cp -rpf Oracle/${OracleDrive} /usr/lcoal/
 fi
-export LD_LIBRARY_PATH=/usr/local/instantclient_19_17:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/${OracleDrive}:$LD_LIBRARY_PATH
 
 go build -o gt-checksum greatdbCheck.go
 chmod +x gt-checksum
 mkdir gt-checksum-${vs}-linux-x86-64
 cp -rpf gt-checksum gc.conf gt-checksum-${vs}-linux-x86-64
-cp -rpf Oracle/instantclient_19_17 gt-checksum-${vs}-linux-x86-64
+cp -rpf Oracle/${OracleDrive} gt-checksum-${vs}-linux-x86-64
 cp -rpf README.md gt-checksum-${vs}-linux-x86-64
 tar zcf gt-checksum-${vs}-linux-x86-64.tar.gz gt-checksum-${vs}-linux-x86-64
 mkdir binary
