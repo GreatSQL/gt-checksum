b57e8a95c3352dfd4fff4d96193e8aa566d6a4a8
YeJinrong <yejr@qq.com>
2025-09-09 14:37:13 +0800

    提高内存检测频率；OOM时退出前清理临时文件并记录内存溢出日志

--- utils/memory_monitor.go
+++ utils/memory_monitor.go
@@ -2,29 +2,31 @@ package utils
 
 import (
 	"fmt"
+	"gt-checksum/global"
+	"gt-checksum/inputArg"
 	"os"
 	"runtime"
 	"strconv"
 	"strings"
 	"time"
-
-
 )
 
 // MemoryMonitor monitors the memory usage of the program asynchronously.
-func MemoryMonitor(memoryLimit string) {
+func MemoryMonitor(memoryLimit string, config *inputArg.ConfigParameter) {
 	limitMB := parseMemoryLimit(memoryLimit)
 	if limitMB == 0 {
 		return
 	}
 
 	go func() {
-		ticker := time.NewTicker(100 * time.Millisecond)
+		ticker := time.NewTicker(50 * time.Millisecond)
 		defer ticker.Stop()
 
 		for range ticker.C {
 			currentMB := getCurrentMemoryUsage()
 			if currentMB >= limitMB {
+				// 清理临时文件并记录日志
+				cleanupTmpFileAndLog(currentMB, limitMB, config)
 				fmt.Printf("\nFatal error: Current memory usage %dMB has reached the limit (%dMB). Exiting...\n", currentMB, limitMB)
 				os.Exit(1)
 			}
@@ -61,4 +63,29 @@ func getCurrentMemoryUsage() int {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 	return int(m.Alloc / 1024 / 1024)
+}
+
+// cleanupTmpFileAndLog 清理临时文件并记录内存溢出日志
+func cleanupTmpFileAndLog(currentMB, limitMB int, config *inputArg.ConfigParameter) {
+	// 使用配置中的临时文件名
+	tmpFile := "tmp_file"
+	if config != nil && config.NoIndexTableTmpFile != "" {
+		tmpFile = config.NoIndexTableTmpFile
+	}
+	
+	if _, err := os.Stat(tmpFile); err == nil {
+		if err := os.Remove(tmpFile); err == nil {
+			if global.Wlog != nil {
+				global.Wlog.Error(fmt.Sprintf("Memory limit exceeded (%dMB/%dMB), %s cleaned up before exit", currentMB, limitMB, tmpFile))
+			}
+		} else {
+			if global.Wlog != nil {
+				global.Wlog.Error(fmt.Sprintf("Memory limit exceeded (%dMB/%dMB), failed to clean up %s: %v", currentMB, limitMB, tmpFile, err))
+			}
+		}
+	} else {
+		if global.Wlog != nil {
+			global.Wlog.Error(fmt.Sprintf("Memory limit exceeded (%dMB/%dMB), no %s found to clean up", currentMB, limitMB, tmpFile))
+		}
+	}
 }
