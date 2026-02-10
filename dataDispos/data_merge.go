6d5d2781ab05a451b173862b746c021968d3b4a3
YeJinrong <yejr@qq.com>
2026-02-10 10:53:17 +0800

    - 支持无聚集索引表 - 支持唯一索引中包含NULL值的场景

--- dataDispos/data_merge.go
+++ dataDispos/data_merge.go
@@ -2,6 +2,7 @@ package dataDispos
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -10,7 +11,7 @@ type DataInfo struct {
 }
 
 /*
-	两个管道的正序数据进行合并排序到一个管道
+两个管道的正序数据进行合并排序到一个管道
 */
 func (sp *DataInfo) ChangeMerge(ch1 <-chan map[string]interface{}, ch2 <-chan map[string]interface{}) chan map[string]interface{} {
 	var cc = make(chan map[string]interface{}, sp.ChanQueueDepth)
@@ -34,6 +35,29 @@ func (sp *DataInfo) ChangeMerge(ch1 <-chan map[string]interface{}, ch2 <-chan ma
 					for k, _ := range v2 {
 						v22 = fmt.Sprintf("%v", k)
 					}
+					// 尝试将值转换为整数进行比较，以便数字类型的索引列能够正确排序
+					if v11 != "END" && v22 != "END" {
+						if v1Int, err1 := strconv.Atoi(v11); err1 == nil {
+							if v2Int, err2 := strconv.Atoi(v22); err2 == nil {
+								// 两个值都是整数，按数字大小比较
+								if v1Int < v2Int {
+									c1 = true
+									c2 = false
+									cc <- v1
+								} else if v1Int == v2Int {
+									c1 = true
+									c2 = true
+									cc <- v1
+								} else {
+									c1 = false
+									c2 = true
+									cc <- v2
+								}
+								continue
+							}
+						}
+					}
+					// 如果不是整数或者转换失败，使用字符串比较
 					if strings.Compare(v11, v22) == -1 {
 						c1 = true
 						c2 = false
@@ -67,11 +91,11 @@ func (sp *DataInfo) ChangeMerge(ch1 <-chan map[string]interface{}, ch2 <-chan ma
 }
 
 /*
-	两个管道的条件，按照指定字符进行先后顺序进行合并，先梳理delete，再梳理insert
+两个管道的条件，按照指定字符进行先后顺序进行合并，先梳理delete，再梳理insert
 */
 func (sp *DataInfo) Merge(ch1 <-chan map[string]interface{}, ch2 <-chan map[string]interface{}, beginST, endST string) chan map[string]interface{} {
 	var cc = make(chan map[string]interface{}, sp.ChanQueueDepth)
-	
+
 	go func() {
 		var ok1, ok2 bool
 		var v1, v2 map[string]interface{}
