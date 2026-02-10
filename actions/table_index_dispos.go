c7698bfe05c443c09a67925fee50c1fc49dc498a
YeJinrong <yejr@qq.com>
2026-02-10 14:25:23 +0800

    更新CHANGELOG;代码对齐

--- actions/table_index_dispos.go
+++ actions/table_index_dispos.go
@@ -1499,9 +1499,9 @@ func (sp *SchedulePlan) AbnormalDataDispos(diffQueryData chanDiffDataS, cc chanS
 													// 构建唯一主键标识
 													primaryKey := fmt.Sprintf("%s.%s.%s", c1.Schema, c1.Table, strings.Join(keyList, ","))
 													// 记录到insertedPrimaryKeys中，加锁保护并发访问
-												insertMutex.Lock()
-												insertedPrimaryKeys[primaryKey] = struct{}{}
-												insertMutex.Unlock()
+													insertMutex.Lock()
+													insertedPrimaryKeys[primaryKey] = struct{}{}
+													insertMutex.Unlock()
 												}
 											}
 
